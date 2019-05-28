1f8fb69cdadc69060c584eed1a5aac3b6d143586
fpestunov <fpestunov@gmail.com>
2019-05-28 15:30:58 +0400

    add FileLoaderTest

---
+++ tests/FileLoaderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Example\FileLoader;
+use PHPUnit\Framework\TestCase;
+
+class FileLoaderTest extends TestCase
+{
+    public function testFileLoaderClassCanBeCreated()
+    {
+        $f = new FileLoader;
+    }
+}
